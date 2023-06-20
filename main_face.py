9c18d16d36b53c261cbf369fe918f419771aa06b
carrdoctor71 <53904775+carrdoctor71@users.noreply.github.com>
2023-06-19 23:28:44 -0400

    Add files via upload

---
+++ main_face.py
@@ -0,0 +1,50 @@
+# project reference: https://www.youtube.com/watch?v=pQvkoaevVMk
+import threading
+import cv2 # camera vision
+from deepface import DeepFace # deep nural network for facial recognition form image
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+counter = 0
+
+face_match = False
+
+reference_img = cv2.imread("reference.jpg")
+
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+        else:
+            face_match = False
+    except ValueError:
+        face_match = False
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
+
+        if face_match:
+            cv2.putText(frame, "Ken", (20,450), cv2.FONT_HERSHEY_COMPLEX, 2, (0,255,0), 3)
+            deposit_amount = input("Welcome Ken, enter your deposit amount:")
+            print("$" + str(deposit_amount + " deposited"))
+        else:
+            cv2.putText(frame, "No Match", (20,450), cv2.FONT_HERSHEY_COMPLEX, 2, (0,0,255), 3)
+
+        cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
